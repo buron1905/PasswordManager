4fb2ffda99ca6b079823831a8784053b10eaa598
buronda <daniel.buron@storaenso.com>
2022-01-27 23:14:50 +0100

    Passwords list page

---
+++ PasswordManager/ViewModels/PasswordsListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PasswordManager.ViewModels
+{
+    public class PasswordsListViewModel : BindableObject
+    {
+        public Action DisplayInvalidLoginPrompt;
+
+        public PasswordsListViewModel()
+        {
+        }
+
+    }
+}
