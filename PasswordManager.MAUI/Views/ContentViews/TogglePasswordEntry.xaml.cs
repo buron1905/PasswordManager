8dec14010dab7d4ae1f5b80806ca377b93f6acb9
buronda <daniel.buron@storaenso.com>
2023-02-28 22:28:49 +0100

    TogglePasswordEntry added, styling Login page

---
+++ PasswordManager.MAUI/Views/ContentViews/TogglePasswordEntry.xaml.cs
@@ -0,0 +1,48 @@
+namespace PasswordManager.MAUI.Views.Controls
+{
+    public partial class TogglePasswordEntry : ContentView
+    {
+        public TogglePasswordEntry()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty PlaceholderProperty =
+            BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(TogglePasswordEntry));
+
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create(nameof(Text), typeof(string), typeof(TogglePasswordEntry),
+                defaultBindingMode: BindingMode.TwoWay);
+
+        public static readonly BindableProperty HidePasswordProperty =
+            BindableProperty.Create(nameof(HidePassword), typeof(bool), typeof(TogglePasswordEntry),
+                defaultValue: true);
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public bool HidePassword
+        {
+            get => (bool)GetValue(HidePasswordProperty);
+            set => SetValue(HidePasswordProperty, value);
+        }
+
+        private void OnImageButtonClicked(object sender, EventArgs e)
+        {
+            HidePassword = !HidePassword;
+        }
+    }
+}
+
+
+
+
