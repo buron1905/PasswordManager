5c3e40eec4d7a7d8347105a4d1a2f3c103a0c0f9
buronda <daniel.buron@storaenso.com>
2023-03-08 17:09:34 +0100

    Revert "playing with dynamic font size for generated password based on lenght and so on..."
    This reverts commit 097c3a6acf865c67f177d3752b2ab1d03acaa233.

--- PasswordManager.Client/src/app/password-generator/password-generator.component.ts
+++ PasswordManager.Client/src/app/password-generator/password-generator.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ClipboardService } from 'ngx-clipboard';
 import { ToastrService } from 'ngx-toastr';
@@ -10,16 +10,14 @@ import { PasswordGeneratorService } from '../services/password-generator.service
   templateUrl: './password-generator.component.html',
   styleUrls: ['./password-generator.component.css']
 })
-export class PasswordGeneratorComponent implements OnInit, OnDestroy, AfterViewInit {
+export class PasswordGeneratorComponent implements OnInit {
   @Output() newPasswordEvent = new EventEmitter<string>();
-  
+
   generatorForm: FormGroup;
   submitted = false;
   loading = false;
 
   generatedPassword: string = "";
-
-  private resizeObserver: ResizeObserver;
 
   constructor(private fb: FormBuilder, private toastrService: ToastrService, private passwordGeneratorService: PasswordGeneratorService, private clipboardService: ClipboardService) {
     this.generatorForm = this.fb.group({
@@ -29,55 +27,11 @@ export class PasswordGeneratorComponent implements OnInit, OnDestroy, AfterViewI
       useUppercase: [true, [Validators.nullValidator]],
       useLowercase: [true, [Validators.nullValidator]],
     });
-    this.resizeObserver = new ResizeObserver(this.scaleFont);
   }
 
   ngOnInit(): void {
     this.onChanges();
     this.generate();
-  }
-
-  ngAfterViewInit(): void {
-    this.resizeObserver.observe(document.getElementById("generatedText"));
-  }
-
-  ngOnDestroy(): void {
-    this.resizeObserver.unobserve(document.getElementById("generatedTextContainer"));
-  }
-
-  lastSetHeight: number;
-
-  scaleFont(): void {
-    let fontSize = document.getElementById("generatedText").style.fontSize;
-    if (typeof fontSize != "string" || fontSize.trim() === "") {
-      document.getElementById("generatedText").style.fontSize = "calc(1.325rem + .9vw)"; // basic fs-2 value from bootstrap
-    }
-    let i: number = 0
-
-    let textHeight = document.getElementById("generatedText").offsetHeight;
-    let containerHeight = document.getElementById("generatedTextContainer").offsetHeight;
-
-    while (textHeight != containerHeight) {
-      if (textHeight > containerHeight - 20) {
-        i--;
-      }
-      else if (textHeight < containerHeight - 50) {
-        i++;
-      }
-
-      document.getElementById("generatedText").style.fontSize = `calc(1.325rem + .9vw + ${i})`;
-      textHeight = document.getElementById("generatedText").offsetHeight;
-    }
-
-    //if (textHeight <= containerHeight)
-    //  return;
-
-    //let ratio = (containerHeight / textHeight).toFixed(2);
-    //if (this.lastSetHeight == textHeight)
-    //  return;
-
-    //document.getElementById("generatedText").style.fontSize = `calc((1.325rem + .9vw) * ${ratio})`;
-    //this.lastSetHeight = document.getElementById("generatedText").offsetHeight;
   }
 
   generate(): void {
