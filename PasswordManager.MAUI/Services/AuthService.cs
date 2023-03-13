be4b9931e997898422ad402a8c4a0ac8f3c065eb
buronda <daniel.buron@storaenso.com>
2023-03-13 12:46:08 +0100

    added services files

---
+++ PasswordManager.MAUI/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using Models.DTOs;
+using Services.Abstraction.Auth;
+
+namespace PasswordManager.MAUI.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<AuthResponseDTO> LoginAsync(LoginRequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResponseDTO> RefreshTokenAsync(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResponseDTO> RegisterAsync(RegisterRequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TokenIsValid(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PasswordManager.MAUI/Services/PasswordService.cs
@@ -0,0 +1,50 @@
+﻿using Models;
+using Models.DTOs;
+using Services.Abstraction.Data;
+using System.Linq.Expressions;
+
+namespace PasswordManager.MAUI.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public Task<bool> AnyAsync(Expression<Func<Password, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PasswordDTO> CreateAsync(Guid userId, PasswordDTO passwordDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid userId, Guid passwordId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Password> FindSingleOrDefaultByCondition(Expression<Func<Password, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PasswordDTO>> GetAllByUserIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PasswordDTO> GetByIdAsync(Guid userId, Guid passwordId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAll(Expression<Func<Password, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PasswordDTO> UpdateAsync(Guid userId, PasswordDTO passwordDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
