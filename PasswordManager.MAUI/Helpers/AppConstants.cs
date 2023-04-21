f2c62829966e41ca2e026962ea31d0cf17c940d2
buronda <daniel.buron@storaenso.com>
2023-04-21 17:32:05 +0200

    updated constants in MAUI to use deployed API

--- PasswordManager.MAUI/Helpers/AppConstants.cs
+++ PasswordManager.MAUI/Helpers/AppConstants.cs
@@ -3,7 +3,7 @@
     public class AppConstants
     {
         // Base
-        public const string ApiUrl = "http://10.0.2.2:5000/api/";
+        public const string ApiUrl = "https://passwordmanagerwebapi.azurewebsites.net/api/";
         //public const string ApiUrl = "https://localhost:5001/api/";
 
         // Auth
