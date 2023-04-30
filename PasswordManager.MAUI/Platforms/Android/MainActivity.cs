aaf049b8f3f623b9e9fd5eadd5cda2dfe8805704
buronda <daniel.buron@storaenso.com>
2023-04-30 12:15:25 +0200

    App icon

--- PasswordManager.MAUI/Platforms/Android/MainActivity.cs
+++ PasswordManager.MAUI/Platforms/Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.Content.PM;
 
 namespace PasswordManager.MAUI
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Label = "Password Manager", Icon = "@drawable/logo", Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
         public override void OnUserInteraction()
